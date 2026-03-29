dcb5671f5c9dbf0ec4dc5a35c210476e41b1cab6
Pencash <151024643+Pencash@users.noreply.github.com>
2026-03-29 22:37:52 +0200

    Fix JSX closing tag issue for div at line 502 by wrapping table content in TabsContent.

---
+++ AdminGivings.tsx
@@ -0,0 +1,8 @@
+<Fragment>
+  {/* Assuming this is part of a larger component */}
+  <TabsContent>
+    <table>
+      {/* Your table content goes here */}
+    </table>
+  </TabsContent>
+</Fragment>
